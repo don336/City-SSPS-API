54b4d4334aa9ee14a939d5afce01bb77daa2fcd4
hriveraperu <gioxshaoran@hotmail.com>
2023-06-24 16:48:50 -0500

    adding the sewerroute in the routes

--- src/Routes/api-v1/routes.js
+++ src/Routes/api-v1/routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import streetRoute from "./street/Street";
+import sewerRoute from "./sewer/Sewer";
+
 const Route = Router();
 
 Route.use("/api/v1/street", streetRoute);
+Route.use("/api/v1/sewer", sewerRoute);
 
 export default Route;
