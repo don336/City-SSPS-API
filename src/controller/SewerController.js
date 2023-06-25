385dfafa8f4c039829154f286512181d628fa411
hriveraperu <gioxshaoran@hotmail.com>
2023-06-24 21:57:31 -0500

    some update

--- src/controller/SewerController.js
+++ src/controller/SewerController.js
@@ -1,84 +1,81 @@
-const Sewer = require('../Models/Sewer'); 
+const Sewer = require('../Models/Sewer');
 
 class SewerController {
-
-    static async getAllSewers(req, res) {
-        try {
-          const sewers = await Sewer.find();
-          res.json(sewers);
-        } catch (error) {
-          res.status(500).json({ error: 'An error occurred while fetching the sewer problem list.' });
-        }
-      }
-    
-      static async getSewerById(req, res) {
-        const { id } = req.params;
-        try {
-          const sewer = await Sewer.findById(id);
-          if (!sewer) {
-            return res.status(404).json({ error: 'Sewer not found.' });
-          }
-          res.json(sewer);
-        } catch (error) {
-          res.status(500).json({ error: 'An error occurred while fetching the sewer Id.' });
-        }
-      }
-      
-    static async createSewer(req, res) {
-      const { city, neighborhood, fullName, email, dateReport, level, comments } = req.body;
-      
-      // Validate required fields
-      if (!city || !neighborhood || !fullName || !email || !dateReport || !level || !comments) {
-        return res.status(400).json({ error: 'Missing required fields.' });
-      }
-  
-      try {
-        const sewer = new Sewer({ city, neighborhood, fullName, email, dateReport, level, comments });
-        await sewer.save();
-        res.status(201).json(sewer);
-      } catch (error) {
-        res.status(500).json({ error: 'An error occurred while creating the sewer.' });
-      }
+  static async getAllSewers(req, res) {
+    try {
+      const sewers = await Sewer.find();
+      res.json(sewers);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the sewer problem list.' });
     }
-  
-    static async updateSewer(req, res) {
-      const { id } = req.params;
-      const { city, neighborhood, fullName, email, dateReport, level, comments } = req.body;
-      
-      // Validate required fields
-      if (!city || !neighborhood || !fullName || !email || !dateReport || !level || !comments) {
-        return res.status(400).json({ error: 'Missing required fields.' });
-      }
-  
-      try {
-        const sewer = await Sewer.findByIdAndUpdate(
-          id,
-          { city, neighborhood, fullName, email, dateReport, level, comments },
-          { new: true }
-        );
-        if (!sewer) {
-          return res.status(404).json({ error: 'Sewer not found.' });
-        }
-        res.json(sewer);
-      } catch (error) {
-        res.status(500).json({ error: 'An error occurred while updating the sewer problem.' });
-      }
-    }
-
-    static async deleteSewer(req, res) {
-        const { id } = req.params;
-        try {
-          const sewer = await Sewer.findByIdAndDelete(id);
-          if (!sewer) {
-            return res.status(404).json({ error: 'Sewer not found.' });
-          }
-          res.sendStatus(204);
-        } catch (error) {
-          res.status(500).json({ error: 'An error occurred while deleting the sewer.' });
-        }
-      }
-  
-    // ...
   }
-  
- export default SewerController;
+
+  static async getSewerById(req, res) {
+    const { id } = req.params;
+    try {
+      const sewer = await Sewer.findById(id);
+      if (!sewer) {
+        return res.status(404).json({ error: 'Sewer not found.' });
+      }
+      res.json(sewer);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the sewer Id.' });
+    }
+  }
+
+  static async createSewer(req, res) {
+    const { city, neighborhood, fullName, email, dateReport, level, comments } = req.body;
+
+    // Validate required fields
+    if (!city || !neighborhood || !fullName || !email || !dateReport || !level || !comments) {
+      return res.status(400).json({ error: 'Missing required fields.' });
+    }
+
+    try {
+      const sewer = new Sewer({ city, neighborhood, fullName, email, dateReport, level, comments });
+      await sewer.save();
+      res.status(201).json(sewer);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while creating the sewer.' });
+    }
+  }
+
+  static async updateSewer(req, res) {
+    const { id } = req.params;
+    const { city, neighborhood, fullName, email, dateReport, level, comments } = req.body;
+
+    // Validate required fields
+    if (!city || !neighborhood || !fullName || !email || !dateReport || !level || !comments) {
+      return res.status(400).json({ error: 'Missing required fields.' });
+    }
+
+    try {
+      const sewer = await Sewer.findByIdAndUpdate(
+        id,
+        { city, neighborhood, fullName, email, dateReport, level, comments },
+        { new: true }
+      );
+      if (!sewer) {
+        return res.status(404).json({ error: 'Sewer not found.' });
+      }
+      res.json(sewer);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while updating the sewer problem.' });
+    }
+  }
+
+  static async deleteSewer(req, res) {
+    const { id } = req.params;
+    try {
+      const sewer = await Sewer.findByIdAndDelete(id);
+      if (!sewer) {
+        return res.status(404).json({ error: 'Sewer not found.' });
+      }
+      res.sendStatus(204);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while deleting the sewer.' });
+    }
+  }
+}
+
+module.exports = SewerController;
