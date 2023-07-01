252f916142e474e6111e95bbd42aa9bc4905648e
hriveraperu <gioxshaoran@hotmail.com>
2023-07-01 16:37:37 -0500

    power route

--- src/Routes/api-v1/routes.js
+++ src/Routes/api-v1/routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import streetRoute from "./street/Street";
 import sewerRoute from "./sewer/Sewer";
+import powerRoute from "./power/Power";
 import authRouter from "./oauthRoute";
 
 const Route = Router();
 
 Route.use("/api/v1/street", streetRoute);
 Route.use("/api/v1/sewer", sewerRoute);
+Route.use("/api/v1/power", powerRoute);
 Route.use("/user", authRouter);
 
 export default Route;
